aa3af5820243a15e5c693369a9aeaf68d44c0c7a
W4ldschr31n <nicolasrich@hotmail.fr>
2024-05-16 16:14:47 +0200

    [ADD] Fake platform

---
+++ Assets/FakePlatform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakePlatform : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float fallSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("ReplayPlayer"))
+        {
+            rb.AddForce(Vector2.down * fallSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
