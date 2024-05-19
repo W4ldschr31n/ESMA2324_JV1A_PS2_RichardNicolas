8b0c3bc2bacafa291f5523f395d960c957643621
W4ldschr31n <nicolasrich@hotmail.fr>
2024-05-19 13:35:46 +0200

    [ADD] resetEnemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,32 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-{
-    private Vector2 initialPosition;
-    private Rigidbody2D rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Start()
-    {
-        initialPosition = transform.position;
-        TimerManager.onTimerEnded.AddListener(OnTimerEnded);
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+{    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<PlayerMovement>().Die();
         }
-    }
-
-    private void OnTimerEnded()
-    {
-        transform.position = initialPosition;
-        rb.velocity = Vector2.zero;
     }
 }
