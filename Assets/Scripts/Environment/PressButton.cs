dce376895e96a6cfa7f681ef084a5daa00a71ff4
W4ldschr31n <nicolasrich@hotmail.fr>
2024-05-30 08:12:17 +0200

    [FIX] button animation

--- Assets/Scripts/Environment/PressButton.cs
+++ Assets/Scripts/Environment/PressButton.cs
@@ -6,6 +6,7 @@ public class PressButton : Activator
 {
     public LayerMask playerLayer;
     private ContactFilter2D playerFilter;
+    private Animator animator;
     [SerializeField] private Collider2D pressSpot;
 
     // Start is called before the first frame update
@@ -13,13 +14,16 @@ public class PressButton : Activator
     {
         playerFilter = new ContactFilter2D();
         playerFilter.SetLayerMask(playerLayer);
+        animator = GetComponent<Animator>();
     }
 
     protected override bool CheckCanActivate()
     {
         List<Collider2D> playersHere = new();
         pressSpot.OverlapCollider(playerFilter, playersHere);
-        return playersHere.Count > 0;
+        bool activated = playersHere.Count > 0;
+        animator.SetBool("Down", playersHere.Count > 0);
+        return activated;
     }
 
     protected override void UpdateDisplay()
