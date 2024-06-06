08d34c6f8278a599a417e1dcbd1aed5e2919188c
W4ldschr31n <nicolasrich@hotmail.fr>
2024-06-06 16:25:13 +0200

    [ADD] parallaxe

--- Assets/Scripts/Global/Parallaxe.cs
+++ Assets/Scripts/Global/Parallaxe.cs
@@ -7,15 +7,13 @@ public class Parallaxe : MonoBehaviour
 {
     // Parameters
     [SerializeField]
-    private float parallaxeFactorX, parallaxeFactorY;
-    [SerializeField]
-    private float repeatDistance; // Set to zero to base on image width
+    private float parallaxeFactorX;
+
 
     // External components
     private Camera mainCamera;
 
     // Data
-    private Vector2 startPosition;
     private const int PPU = 128; // Pixels per unit
 
     private void Awake()
@@ -23,63 +21,14 @@ public class Parallaxe : MonoBehaviour
         mainCamera = FindObjectOfType<Camera>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        startPosition = transform.position;
-        if (repeatDistance <= 0)
-        {
-            SpriteRenderer sprite = GetComponent<SpriteRenderer>();
-            if (sprite)
-            {
-                repeatDistance = sprite.bounds.size.x;
-            }
-            else
-            {
-                TilemapRenderer tile = GetComponent<TilemapRenderer>();
-                repeatDistance = tile.bounds.size.x;
-            }
-        }
-    }
-
     // Update is called once per frame
     void LateUpdate()
     {
-        UpdateParallaxeX();
-
-        if (parallaxeFactorY >= 0)
-        {
-            UpdateParallaxeY();
-        }
-    }
-
-    private void UpdateParallaxeX()
-    {
-        // Move relatively to the camera, inv. proportionaly to the parallax factor (1 = follow camera, 0 = don't move)
-        float distance = mainCamera.transform.position.x * parallaxeFactorX;
-        Vector3 newPosition = new Vector3(startPosition.x + distance, transform.position.y, transform.position.z);
+        Vector3 newPosition = new Vector3(mainCamera.transform.position.x * (1 - parallaxeFactorX), transform.position.y, transform.position.z);
         transform.position = PixelPerfectClamp(newPosition);
 
-        // Move the sprite when it reaches the middle of screen
-        float breakPoint = mainCamera.transform.position.x * (1 - parallaxeFactorX);
-        if (breakPoint > startPosition.x + (repeatDistance / 2))
-        {
-            startPosition.x += repeatDistance;
-        }
-        else if (breakPoint < startPosition.x - (repeatDistance / 2))
-        {
-            startPosition.x -= repeatDistance;
-        }
     }
 
-
-    private void UpdateParallaxeY()
-    {
-        float distance = mainCamera.transform.position.y * parallaxeFactorY;
-        Vector3 newPosition = new Vector3(transform.position.x, startPosition.y + distance, transform.position.z);
-        transform.position = PixelPerfectClamp(newPosition);
-        // Don't move the sprite if it leaves the screen
-    }
 
     private Vector3 PixelPerfectClamp(Vector3 vectorToClamp)
     {
